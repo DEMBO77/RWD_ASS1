5b9f89f427b9759e45a922b7a5c861c7c6be7d52
remi <remi.vandelle@outlook.fr>
2019-11-26 15:14:04 +0100

    Double click update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
+
+$(window).dblclick(function(event){
+    var x = event.pageX;
+    var y = event.pageY;
+    console.log(x+" "+y);
+
+    var imgOrangeFish = $('#fish1Id');
+    var pos = imgOrangeFish.position();
+    var width = imgOrangeFish.width();
+    var height = imgOrangeFish.height();
+    if ((pos.left<x) && (x<(pos.left+width))){
+        if (((pos.top+height)>y) && (y>pos.top)){
+
+            imgOrangeFish.width(350);
+            imgOrangeFish.height(350);
+            setTimeout(function(){
+                imgOrangeFish.width(250);
+                imgOrangeFish.height(250);
+                },1000);
+        }
+    }
+
+
+
+});
+
+
 $(window).click(function(event){
     var x = event.pageX;
     var y = event.pageY;
     console.log(x+" "+y);
-    $("#fish1Id").animate({top: y, left: x});
+    $("#fish1Id").animate({top: y-125, left: x-125});
 });
+
+
+
+
